888138d8b699248c069c5b1b844fc1de8f481338
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-01-19 19:12:31 +0530

    Add support for threaded messages
    Also mention who's being OOCed

--- index.js
+++ index.js
@@ -9,14 +9,16 @@ const outOfContextChannelID = 'C01270P3XFV'
 const outOfContextSandboxChannelID = 'C01JXGVB99U'
 
 app.event('message', async ({ event, client }) => {
-    console.log(event)
     if (
         (event.channel === outOfContextChannelID
             || event.channel === outOfContextSandboxChannelID)
         && event.attachments && event.attachments[0].is_share && event.attachments[0].is_msg_unfurl
     ) {
+
+        const messageThreadMatch = event.attachments[0].from_url.match(/thread_ts=(.*)&/)
+
         let inChannel = event.attachments[0].channel_id
-        let ts = event.attachments[0].ts
+        let ts = messageThreadMatch ? messageThreadMatch[1].slice(0, -6) + '.' + messageThreadMatch[1].slice(-6) : event.attachments[0].ts
         let outOfContexter = event.user
 
         const response = await client.chat.getPermalink({
@@ -31,7 +33,7 @@ app.event('message', async ({ event, client }) => {
 
         await client.chat.postMessage({
             channel: inChannel,
-            text: `You were OOCed by <@${outOfContexter}>! ${response.permalink}`,
+            text: `<@${event.attachments[0].author_id}> was OOCed by <@${outOfContexter}>! ${response.permalink}`,
             thread_ts: ts,
             unfurl_links: false,
             unfurl_media: false
